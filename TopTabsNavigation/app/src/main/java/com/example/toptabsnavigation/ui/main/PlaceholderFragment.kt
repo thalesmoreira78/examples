a26d3d670000053357e7d0cbbdd90c3da9afbb09
Thales <thales.moreira@iteris.com.br>
2020-11-17 13:52:58 -0300

    Criacao dos botoes com redirecionamento para as telas OptionOne e OptionTwo

--- TopTabsNavigation/app/src/main/java/com/example/toptabsnavigation/ui/main/PlaceholderFragment.kt
+++ TopTabsNavigation/app/src/main/java/com/example/toptabsnavigation/ui/main/PlaceholderFragment.kt
@@ -1,14 +1,17 @@
 package com.example.toptabsnavigation.ui.main
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.toptabsnavigation.OptionOneActivity
 import com.example.toptabsnavigation.OptionTwoScrollingActivity
@@ -28,32 +31,50 @@ class PlaceholderFragment : Fragment() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
+        val button1 = createButton("Option 1", Intent(context, OptionOneActivity::class.java));
+        val button2 = createButton("Name", Intent(context, OptionOneActivity::class.java));
+        val button3 = createButton("Name", Intent(context, OptionTwoScrollingActivity::class.java));
+        val button4 = createButton("Option 2",Intent(context, OptionTwoScrollingActivity::class.java));
+        val layout = root.findViewById(R.id.fragmentMainLinearLayout) as LinearLayout
+        layout.addView(button1)
+        layout.addView(button2)
+        layout.addView(button3)
+        layout.addView(button4)
 
-        val buttonOptionOne: Button = root.findViewById(R.id.buttonOptionOne)
-        buttonOptionOne.setOnClickListener { view ->
-            val intent = Intent(context, OptionOneActivity::class.java)
-                // .apply { putExtra("message", "Hello, world!") }
-            startActivity(intent)
-        }
-
-
-        val buttonOptionTwo: Button = root.findViewById(R.id.buttonOptionTwo)
-
-        buttonOptionTwo.setOnClickListener { view ->
-            val intent = Intent(context, OptionTwoScrollingActivity::class.java)
-                // .apply { putExtra("message", "Hello, world!") }
-            startActivity(intent)
-        }
         return root
+    }
+
+    fun createButton(text: String, intent: Intent): Button {
+        val button = Button(context)
+        // setting layout_width and layout_height using layout parameters
+        button.layoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        button.text = text
+
+        val drawableLeft = ResourcesCompat.getDrawable(resources, R.drawable.ic_action_album, null);
+        val drawableLeftH = drawableLeft!!.intrinsicHeight
+        val drawableLeftW = drawableLeft!!.intrinsicWidth
+        drawableLeft.setBounds(0, 0, drawableLeftW, drawableLeftH)
+
+        val drawableRight =
+            ResourcesCompat.getDrawable(resources, R.drawable.ic_action_star_full, null);
+        val drawableRightH = drawableRight!!.intrinsicHeight
+        val drawableRightW = drawableRight!!.intrinsicWidth
+        drawableRight.setBounds(0, 0, drawableRightW, drawableRightH)
+
+        button.setCompoundDrawables(drawableLeft, null, drawableRight, null);
+        button.setOnClickListener {
+            startActivity(intent)
+        }
+        return button;
     }
 
     companion object {
