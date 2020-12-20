d68d18dfbbdada5bab8f31b846a43551d53d58d2
Thales <thales.moreira@iteris.com.br>
2020-12-20 10:35:46 -0300

    codigo com erro

--- TopTabsNavigation/app/src/main/java/com/example/toptabsnavigation/ui/optionone/OptionOnePlaceholderFragment.kt
+++ TopTabsNavigation/app/src/main/java/com/example/toptabsnavigation/ui/optionone/OptionOnePlaceholderFragment.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -31,7 +31,24 @@ class OptionOnePlaceholderFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_option_one, container, false)
+        val layout = root.findViewById(R.id.fragmentOneConstraintLayout) as ConstraintLayout
         val textView: TextView = root.findViewById(R.id.section_label_option_one)
+
+        val sectionNumber = arguments?.getInt(ARG_SECTION_NUMBER) ?: 1
+
+        if (sectionNumber == 1) {
+            val listView = ListView(context)
+            // setting layout_width and layout_height using layout parameters
+            listView.layoutParams = ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            val language = listOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
+            val arrayAdapter = ArrayAdapter<String>(root.context, R.layout.list_view, language)
+            listView.adapter = arrayAdapter
+            layout.addView(listView)
+        }
+
         pageViewModel.text.observe(this, Observer<String> {
             textView.text = it
         })
